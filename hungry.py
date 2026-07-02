c813e585463d18150b92bc7080a1f61374c2af92
Ifeanyi-07 <abahobiajulu@gmail.com>
2026-07-02 11:32:33 +0100

    made extra changes and added prompt

--- hungry.py
+++ hungry.py
@@ -1,1 +1,7 @@
-print("i am hungry")
+print("i am hungry but i want to loose weight")
+
+hun = input("Are you hungry? :")
+if hun == "yes":
+    print("Come and take cake")
+else:
+    print("Go to the gym then")
